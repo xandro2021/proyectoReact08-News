3ef1affd5919f4dbab3c0a19a338b91e83751be0
xandro2021 <xandrocastro2021@gmail.com>
2023-11-04 14:10:23 -0600

    use pagination and link it with an state

--- src/components/ListadoNoticias.jsx
+++ src/components/ListadoNoticias.jsx
@@ -1,10 +1,14 @@
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
+import Pagination from '@mui/material/Pagination';
+import Stack from '@mui/material/Stack';
 import useNoticias from '../hooks/useNoticias';
 import Noticia from '../components/Noticia';
+import { generarId } from '../helpers';
 
 const ListadoNoticias = () => {
-  const { noticias } = useNoticias();
+  const { noticias, totalNoticias, handleChangePagina, pagina } = useNoticias();
+  const totalPaginas = Math.ceil(totalNoticias / 20);
 
   return (
     <>
@@ -23,11 +27,26 @@ const ListadoNoticias = () => {
       >
         {noticias.map(noticia => (
           <Noticia
-            key={noticia.url}
+            key={generarId()}
             noticia={noticia}
           />
         ))}
       </Grid>
+
+      <Stack
+        spacing={2}
+        direction={'row'}
+        justifyContent="center"
+        alignItems="center"
+        marginY={5}
+      >
+        <Pagination
+          count={totalPaginas}
+          color="primary"
+          onChange={handleChangePagina}
+          page={pagina}
+        />
+      </Stack>
     </>
   );
 };
--- src/context/NoticiasProvider.jsx
+++ src/context/NoticiasProvider.jsx
@@ -6,6 +6,8 @@ const NoticiasContext = createContext();
 const NoticiasProvider = ({ children }) => {
   const [categoria, setCategoria] = useState('general');
   const [noticias, setNoticias] = useState([]);
+  const [pagina, setPagina] = useState(1);
+  const [totalNoticias, setTotalNoticias] = useState(0);
 
   useEffect(() => {
     const consultarAPI = async () => {
@@ -13,13 +15,31 @@ const NoticiasProvider = ({ children }) => {
 
       const { data } = await axios(url);
       setNoticias(data.articles);
+      setTotalNoticias(data.totalResults);
     };
 
     consultarAPI();
   }, [categoria]);
 
+  useEffect(() => {
+    const consultarAPI = async () => {
+      const url = `https://newsapi.org/v2/top-headlines?country=us&page=${pagina}&category=${categoria}&apiKey=${import.meta.env.VITE_API_KEY}`;
+
+      const { data } = await axios(url);
+      setNoticias(data.articles);
+      setTotalNoticias(data.totalResults);
+    };
+
+    consultarAPI();
+  }, [pagina]);
+
   const handleChangeCategoria = e => {
+    setPagina(1);
     setCategoria(e.target.value);
+  };
+
+  const handleChangePagina = (e, valor) => {
+    setPagina(valor)
   };
 
   return (
@@ -28,6 +48,9 @@ const NoticiasProvider = ({ children }) => {
         categoria,
         handleChangeCategoria,
         noticias,
+        totalNoticias,
+        handleChangePagina,
+        pagina,
       }}
     >
       {children}
